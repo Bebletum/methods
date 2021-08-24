71141139c7e266b7e08d3d3199b2eb3d69d03ad9
Bebletum <86402231+Bebletum@users.noreply.github.com>
2021-08-24 23:27:59 +0300

    1

---
+++ Lab1/Airlines/Entities/Airline.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airlines
+{
+    class Airline
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private List<Airport> _airports;
+
+        public List<Airport> Airports
+        {
+            get { return _airports; }
+            set { _airports = value; }
+        }
+
+
+        public Airline()
+        {
+            _airports = new List<Airport>();
+        }
+
+        public List<Flight> GetAllFlights()
+        {
+            List<Flight> flights = new List<Flight>();
+            foreach (var a in Airports)
+            {
+                foreach (var fl in a.Flights)
+                {
+                    flights.Add(fl);
+                }
+            }
+            return flights;
+        }
+
+        public List<Flight> GetFlightsBetweenTownsByDate(string startTown, string destinationTown, DateTime startDate, DateTime endDate)
+        {
+            List<Flight> flights = new List<Flight>();
+            foreach (var a in Airports)
+            {
+                foreach (var f in a.Flights)
+                {
+                    if(f.StartTown == startTown &&
+                       f.DestinationTown == destinationTown &&
+                       f.Date >= startDate &&
+                       f.Date <= endDate)
+                    {
+                        flights.Add(f);
+                    }
+                }
+            }
+            return flights;
+        }
+
+        public List<Ticket> GetTicketsByFlightNumber(string number)
+        {
+            List<Ticket> tickets = new List<Ticket>();
+            foreach (var a in Airports)
+            {
+                foreach (var t in a.Tickets)
+                {
+                    if (t.Flight.Number == number)
+                        tickets.Add(t);
+                }
+            }
+            return tickets;
+        }
+    }
+}
